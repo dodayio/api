55d19ed4e39a15d8ea2f71a70ff69368bbf14223
Смирнов Иван Александрович <v-ivsmir@microsoft.com>
2019-02-08 23:00:44 +0300

    added colors to tags

--- src/seed/seed-mutations.js
+++ src/seed/seed-mutations.js
@@ -1,21 +1,21 @@
 export default /* GraphQL seed db */ `
   mutation {
-    c1: CreateTag(id: "100", sysname: "health") {
+    c1: CreateTag(id: "100", sysname: "health", color: "#B6E060") {
       id
     }
-    c2: CreateTag(id: "101", sysname: "selfdev") {
+    c2: CreateTag(id: "101", sysname: "selfdev", color: "#FFE86C") {
       id
     }
-    c3: CreateTag(id: "102", sysname: "career") {
+    c3: CreateTag(id: "102", sysname: "career", color: "#A772CB") {
       id
     }
-    c4: CreateTag(id: "103", sysname: "relationship") {
+    c4: CreateTag(id: "103", sysname: "relationship", color: "#E6007C") {
       id
     }
-    c5: CreateTag(id: "104", sysname: "prosperity") {
+    c5: CreateTag(id: "104", sysname: "prosperity", color: "#FF4E00") {
       id
     }
-    c6: CreateTag(id: "105", sysname: "hobby") {
+    c6: CreateTag(id: "105", sysname: "hobby", color: "#009DE6") {
       id
     }
     r1: CreateRole(id: "200", sysname: "founder") {
